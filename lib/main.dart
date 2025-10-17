2cbdc8e7645a89be97635dafe011eb45d24c7fa0
Syan Par Chan <syanparchan@gmail.com>
2025-10-17 14:22:24 +0630

    notification permission, notification messages send ,receive successfully

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:cooking/screen/add_cooking_menu.dart';
 import 'package:cooking/screen/signupscreen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 
 import 'firebase_options.dart';
@@ -9,7 +10,36 @@ import 'firebase_options.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  NotificationSettings settings = await FirebaseMessaging.instance
+      .requestPermission(
+        alert: true,
+        announcement: false,
+        badge: true,
+        criticalAlert: false,
+        provisional: false,
+        sound: false,
+      );
+
+  print('User granted premission:${settings.authorizationStatus}');
+  FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+    print('Got a message whilist in the foreground');
+    print('Message data: ${message.data}');
+
+    if (message.notification != null) {
+      print(
+        'Message also contained a notification: ${message.notification?.title}',
+      );
+    }
+  });
+
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
   runApp(const MyApp());
+}
+
+@pragma('vm:entry-point')
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp();
+  print("Handling a background message: ${message.messageId}");
 }
 
 class MyApp extends StatelessWidget {
